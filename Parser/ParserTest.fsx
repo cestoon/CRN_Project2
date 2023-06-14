33b7ff39ab9e8d343e118693d65385048b935dd1
fengbingkun001 <fengbingkun001@ke.com>
2023-06-14 08:37:59 +0200

    del ast.fs

--- Parser/ParserTest.fsx
+++ Parser/ParserTest.fsx
@@ -5,17 +5,48 @@ open Parser;;
 
 open FParsec
 
-
-let gcd = "crn = {
-    conc[a,a0], 
-    conc[b,b0], 
-    step[{ 
-        ld[a, atmp], 
-        ld[b, btmp],
-        cmp[a,b] 
-    }], 
+let gcd = "crn={
+    conc[b,32 ],
+    conc[a,12 ],
     step[{
-        ifGT[{ sub[atmp,btmp,a] }],
-        ifLT[{ sub[btmp,atmp,b] }] 
-    }] 
+    ld[a, atmp],
+    ld[b, btmp],
+    cmp[a,b]
+    }],
+    step[{
+     ifGT[{ sub[atmp,btmp,a] }],
+     ifLT[{ sub[btmp,atmp,b] }]
+     }]
+     };"
+
+let p1 = "crn={ conc[b, 32], conc[a, 12]}"
+
+let discreteCounter = "crn={
+ conc[c,2], conc[cInitial, 4],
+ conc[one ,1], conc[zero ,0],
+ step[{
+ sub[c,one,cnext ],
+ cmp[c,zero]
+ }],
+ step[{
+ ifGT[{ ld[cnext ,c] }],
+ ifLE[{ ld[ cInitial ,c] }]
+ }]
 };"
+
+let fac =  "crn={
+ conc[ f ,1], conc[one ,1], conc[ i , 5 ],
+ step[{
+ cmp[i,one ],
+ mul[f , i , fnext ],
+ sub[ i ,one, inext ]
+ }],
+ step[{
+ ifGT[{
+ ld[ inext , i ],
+ ld[ fnext , f ]
+ }]
+ }]
+};"
+
+parseString gcd    
--- Parser/parser.fs
+++ Parser/parser.fs
@@ -79,18 +79,18 @@ let pCrn : Parser<Crn, unit> =
 
 let parseString = run (pCrn  .>>  eof) 
 
-let gcd = "crn = {
-    conc[a,a0], 
-    conc[b,b0], 
-    step[{ 
-        ld[a, atmp], 
-        ld[b, btmp],
-        cmp[a,b] 
-    }], 
-    step[{
-        ifGT[{ sub[atmp,btmp,a] }],
-        ifLT[{ sub[btmp,atmp,b] }] 
-    }] 
-};"
+// let gcd = "crn = {
+//     conc[a,a0], 
+//     conc[b,b0], 
+//     step[{ 
+//         ld[a, atmp], 
+//         ld[b, btmp],
+//         cmp[a,b] 
+//     }], 
+//     step[{
+//         ifGT[{ sub[atmp,btmp,a] }],
+//         ifLT[{ sub[btmp,atmp,b] }] 
+//     }] 
+// };"
 
-parseString gcd
+// parseString gcd
